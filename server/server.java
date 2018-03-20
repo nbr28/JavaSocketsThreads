5632ffad99d6952f226cb6393d95774b3a783a50
U-note2\y2s1982 <y2s1982@gmail.com>
2018-03-20 08:02:13 -0400

    first iteration of the threaded server

--- server/server.java
+++ server/server.java
@@ -6,83 +6,45 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 import BankClasses.Bank;
 import BankClasses.BankApp;
 
 public class server {
+	private static Bank bank;
+	private static ServerSocket serverSocket;
+	private static ArrayList<Thread> clients;
 
 	public static void main(String[] args) {
-		Bank bank = new Bank();
+		bank = new Bank();
 		BankApp.loadBank(bank);// load some accounts
-		ServerSocket serverSocket; // TCP socket used for listening
 
 		try {
-			/*
-			 * step 1: create a server socket port number: 8000
-			 */
+			try { /* allows finally to close socket */
+				/* create ServerSocket object */
+				serverSocket = new ServerSocket(8000);
 
-			serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+				/* log initialization */
+				System.out.println("*** R&S Bank Server System ***");
 
-			/*
-			 * setp 2: listen for a connection and create a socket
-			 */
-			System.out.println("*** this server is going register the cars ***");
-			System.out.println("listening for a connection...");
-
-			Socket socketConnection = serverSocket.accept(); // listen and wait
-																// TCP socket that is connected
-																// to the client
-
-			/*
-			 * step 3: connect input and output streams to the socket
-			 */
-			ObjectOutputStream oosToClient = new ObjectOutputStream(socketConnection.getOutputStream());
-
-			ObjectInputStream oisFromClient = new ObjectInputStream(socketConnection.getInputStream());
-
-			System.out.println("I/O streams connected to the socket");
-
-			/*
-			 * step 4: exchange objects with THE client
-			 */
-
-			try {
-				int m = 1000;
-				while(true)
-				{
-					String string=(String)oisFromClient.readObject();
-					System.out.println(string);
-					
+				/* listen for new connection forever, pushing each thread into ArrayList */
+				for (;;) {
+					System.out.println("listening for a connection...");
+					Thread t = new serverThread(serverSocket.accept(), bank);
+					t.start();
+					clients.add(t);
 				}
-				
-				//oosToClient.writeObject(bank.searchByAccountNumber("A1234"));
-				
-
-				// display the result to the screen of the server
-				//System.out.println("\t******* send the object to the CLIENT");
-
+			} finally {
+				System.out.println("*** the server is going to stop running ***");
+				serverSocket.close();
 			}
-			catch (EOFException eof) {
-				System.out.println("*** THE client has terminated connection ***");
-				eof.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-
-			/*
-			 * step 5: close the connection to the client
-			 */
-			oosToClient.close();
-			oisFromClient.close();
-			socketConnection.close();
+		} catch (EOFException eof) {
+			System.out.println("*** THE client has terminated connection ***");
+			eof.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-		System.out.println("*** the server is going to stop running ***");
 
 	} // end main
 }
