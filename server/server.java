a95f2b54dca2f3759e0c8fa3b2004dd4293bba27
U-note2\y2s1982 <y2s1982@gmail.com>
2018-03-20 08:27:35 -0400

    commented out ArrayList. Re-implement after learning how to work with threads in ArrayList

--- server/server.java
+++ server/server.java
@@ -14,7 +14,7 @@ import BankClasses.BankApp;
 public class server {
 	private static Bank bank;
 	private static ServerSocket serverSocket;
-	private static ArrayList<Thread> clients;
+//	private static ArrayList<Thread> clients;
 
 	public static void main(String[] args) {
 		bank = new Bank();
@@ -33,7 +33,7 @@ public class server {
 					System.out.println("listening for a connection...");
 					Thread t = new serverThread(serverSocket.accept(), bank);
 					t.start();
-					clients.add(t);
+//					clients.add(t);
 				}
 			} finally {
 				System.out.println("*** the server is going to stop running ***");
